097f4094f9cc266479bc25ba6da544d997681334
chrislpatton <chrislpatton@gmail.com>
2016-02-12 00:56:48 -0500

    a commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('./server.js');
 
- var port = 4568;
-
+ //var port = 4568;
+var port = process.env.PORT || 4568;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
